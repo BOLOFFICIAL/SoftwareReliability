461a14de3dedcf509229c33b8b56e45f5c333d00
Bolonin Official <boloninnn@gmail.com>
2023-10-12 19:03:54 +0300

    Laba2

---
+++ Laba2/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Button();
+            this.number_of_experiments = new System.Windows.Forms.NumericUpDown();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.failure_rate = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.number_of_experiments)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.failure_rate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(64)))), ((int)(((byte)(221)))), ((int)(((byte)(64)))));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.chart, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.number_of_experiments, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.result, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.failure_rate, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Интенсивность отказа";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Количество экспериментов";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.Color.Green;
+            this.result.Dock = System.Windows.Forms.DockStyle.Top;
+            this.result.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.ForeColor = System.Drawing.SystemColors.Menu;
+            this.result.Location = new System.Drawing.Point(10, 146);
+            this.result.Margin = new System.Windows.Forms.Padding(10);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(200, 40);
+            this.result.TabIndex = 3;
+            this.result.Text = "Результат";
+            this.result.UseVisualStyleBackColor = false;
+            this.result.Click += new System.EventHandler(this.res_Click);
+            // 
+            // number_of_experiments
+            // 
+            this.number_of_experiments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.number_of_experiments.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.number_of_experiments.Location = new System.Drawing.Point(3, 107);
+            this.number_of_experiments.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.number_of_experiments.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.number_of_experiments.Name = "number_of_experiments";
+            this.number_of_experiments.Size = new System.Drawing.Size(214, 26);
+            this.number_of_experiments.TabIndex = 5;
+            this.number_of_experiments.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.number_of_experiments.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.number_of_experiments.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // chart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea2);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart.Location = new System.Drawing.Point(223, 3);
+            this.chart.Name = "chart";
+            this.tableLayoutPanel1.SetRowSpan(this.chart, 6);
+            series3.ChartArea = "ChartArea1";
+            series3.Name = "Гистограмма";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Name = "Расчётный график";
+            this.chart.Series.Add(series3);
+            this.chart.Series.Add(series4);
+            this.chart.Size = new System.Drawing.Size(574, 444);
+            this.chart.TabIndex = 6;
+            this.chart.Text = "chart1";
+            // 
+            // failure_rate
+            // 
+            this.failure_rate.AutoSize = true;
+            this.failure_rate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.failure_rate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.failure_rate.Location = new System.Drawing.Point(3, 39);
+            this.failure_rate.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.failure_rate.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.failure_rate.Name = "failure_rate";
+            this.failure_rate.Size = new System.Drawing.Size(214, 26);
+            this.failure_rate.TabIndex = 4;
+            this.failure_rate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.failure_rate.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.failure_rate.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.number_of_experiments)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.failure_rate)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.NumericUpDown number_of_experiments;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.NumericUpDown failure_rate;
+    }
+}
+
---
+++ Laba2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Laba2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            chart.Series[1].Color = Color.Red;
+            chart.Series[1].BorderWidth = 2;
+        }
+
+        private void res_Click(object sender, EventArgs e)
+        {
+            // Вызываем метод PerformCalculations при нажатии кнопки "Рассчитать"
+            PerformCalculations();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            // Вызываем метод PerformCalculations при изменении значения в первом числовом поле
+            PerformCalculations();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            // Вызываем метод PerformCalculations при изменении значения во втором числовом поле
+            PerformCalculations();
+        }
+
+        private void PerformCalculations()
+        {
+            // Очищаем точки в первой и второй сериях графика перед вычислениями
+            chart.Series[0].Points.Clear();
+            chart.Series[1].Points.Clear();
+
+            // Создаем объект для генерации случайных чисел
+            Random random = new Random();
+            // Получаем значение λ (интенсивность отказов) из текстового поля
+            double λ = double.Parse(failure_rate.Text);
+            // Получаем количество экспериментов из текстового поля
+            double numExperiments = double.Parse(number_of_experiments.Text);
+            List<double> list = new List<double>();
+
+            // Генерируем случайные значения и заполняем список
+            for (int i = 0; i < numExperiments; i++)
+            {
+                // Генерируем случайное число и добавляем его в список
+                list.Add(-Math.Log(1 - random.NextDouble()) / λ);
+            }
+
+            // Находим максимальное значение в списке
+            double maxNum = list.Max();
+            // Вычисляем интервал для гистограммы
+            double interval = maxNum / 20;
+            double c = 0;
+
+            // Выполняем вычисления и заполняем график данными
+            while (c <= maxNum)
+            {
+                double hitRate = 0;
+                // Подсчитываем, сколько значений попадает в текущий интервал
+                foreach (var timeFromList in list)
+                {
+                    if (timeFromList >= c && timeFromList < c + interval)
+                    {
+                        hitRate++;
+                    }
+                }
+
+                // Вычисляем частоту событий в интервале
+                double freq = hitRate / numExperiments;
+                // Вычисляем вероятность события в интервале
+                double p = freq / interval;
+
+                List<double> counter = new List<double>();
+                for (int i = 0; i <= maxNum; i++)
+                {
+                    // Рассчитываем теоретическую вероятность события
+                    counter.Add(λ * Math.Exp(-λ * c));
+                }
+
+                // Добавляем точку на графике для текущего интервала
+                chart.Series[0].Points.AddXY(c + interval / 2, p);
+
+                for (int i = 0; i <= maxNum; i++)
+                {
+                    // Добавляем точку на графике для теоретической вероятности
+                    chart.Series[1].Points.AddXY(c, counter[i]);
+                }
+
+                c += interval;
+            }
+        }
+    }
+}
