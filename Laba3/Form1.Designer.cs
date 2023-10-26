cb8f6516b6d5580cafb793d8d1c2f7f442f40b0c
Bolofficial <boloninnn@gmail.com>
2023-10-26 22:26:42 +0300

    Laba3

---
+++ Laba3/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox_Time = new System.Windows.Forms.TextBox();
+            this.textBox_FailureRate = new System.Windows.Forms.TextBox();
+            this.textBox_NumberExperiments = new System.Windows.Forms.TextBox();
+            this.button_res = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.chart1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button_res, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_Time);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 43);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Время";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_FailureRate);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 52);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 39);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Интенсивность отказов";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox_NumberExperiments);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(3, 97);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 39);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Количество экспериментов";
+            // 
+            // textBox_Time
+            // 
+            this.textBox_Time.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Time.Location = new System.Drawing.Point(3, 16);
+            this.textBox_Time.Name = "textBox_Time";
+            this.textBox_Time.Size = new System.Drawing.Size(194, 20);
+            this.textBox_Time.TabIndex = 0;
+            this.textBox_Time.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_FailureRate
+            // 
+            this.textBox_FailureRate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_FailureRate.Location = new System.Drawing.Point(3, 16);
+            this.textBox_FailureRate.Name = "textBox_FailureRate";
+            this.textBox_FailureRate.Size = new System.Drawing.Size(194, 20);
+            this.textBox_FailureRate.TabIndex = 1;
+            this.textBox_FailureRate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_NumberExperiments
+            // 
+            this.textBox_NumberExperiments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_NumberExperiments.Location = new System.Drawing.Point(3, 16);
+            this.textBox_NumberExperiments.Name = "textBox_NumberExperiments";
+            this.textBox_NumberExperiments.Size = new System.Drawing.Size(194, 20);
+            this.textBox_NumberExperiments.TabIndex = 1;
+            this.textBox_NumberExperiments.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button_res
+            // 
+            this.button_res.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_res.Location = new System.Drawing.Point(3, 142);
+            this.button_res.Name = "button_res";
+            this.button_res.Size = new System.Drawing.Size(200, 23);
+            this.button_res.TabIndex = 2;
+            this.button_res.Text = "Расчет";
+            this.button_res.UseVisualStyleBackColor = true;
+            this.button_res.Click += new System.EventHandler(this.button_res_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(209, 3);
+            this.chart1.Name = "chart1";
+            this.tableLayoutPanel1.SetRowSpan(this.chart1, 5);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Name = "Диаграмма частот";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Name = "Расчётный график";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(588, 444);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox_NumberExperiments;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_Time;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_FailureRate;
+        private System.Windows.Forms.Button button_res;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Laba3/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Laba3
+{
+    public partial class Form1 : Form
+    {
+        private double _time = 0;        // Время
+        private double _failureRate = 0; // Интенсивность отказов
+        private double _numberExperiments = 0; // Количество экспериментов
+        private Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+        }
+
+        private void button_res_Click(object sender, EventArgs e)
+        {
+            if (ValidationInput())
+            {
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+
+                List<int> hits = new List<int>();
+                for (int i = 0; i < _numberExperiments; i++)
+                {
+                    int hitCount = CalculateHitCount(); // Вычисление количества отказов в одном эксперименте
+                    hits.Add(hitCount);
+                }
+
+                int maxHits = hits.Max();
+                List<double> list = new List<double>();
+
+                for (int i = 0; i < maxHits; i++)
+                {
+                    int hitCount = hits.Count(h => h == i); // Подсчет количества экспериментов с i отказами
+                    list.Add((double)hitCount / _numberExperiments); // Вычисление вероятности i отказов
+                }
+
+                for (int i = 0; i < maxHits; i++)
+                {
+                    chart1.Series[0].Points.AddXY(i, list[i]);
+                    double counter = CalculateCounter(i); // Вычисление значения "counter"
+                    chart1.Series[1].Points.AddXY(i, counter);
+                }
+            }
+        }
+
+        // Метод для вычисления количества отказов в одном эксперименте
+        private int CalculateHitCount()
+        {
+            double expt = (-Math.Log(1 - _random.NextDouble()) / _failureRate); // Генерация времени до первого отказа
+            int hitCount = 0;
+
+            while (expt < _time) // Пока время до отказа меньше заданного времени
+            {
+                hitCount++;
+                expt += (-Math.Log(1 - _random.NextDouble()) / _failureRate); // Генерация времени до следующего отказа
+            }
+
+            return hitCount;
+        }
+
+        // Метод для вычисления значения "counter"
+        private double CalculateCounter(int i)
+        {
+            return (Math.Pow(_failureRate * _time, i) / Factorial(i)) * Math.Exp(-_failureRate * _time);
+        }
+
+        // Метод для проверки корректности входных данных
+        private bool ValidationInput()
+        {
+            if (!double.TryParse(textBox_Time.Text, out _time))
+            {
+                MessageBox.Show("Некорректное значение для поля Время");
+                return false;
+            }
+            else 
+            {
+                _time = Math.Abs(_time);
+                if (_time > 1)
+                {
+                    MessageBox.Show("Время вне диаппазона допустимых значений");
+                    return false;
+                }
+            }
+            if (!double.TryParse(textBox_FailureRate.Text, out _failureRate))
+            {
+                MessageBox.Show("Некорректное значение для поля Интенсивность отказов");
+                return false;
+            }
+            else 
+            {
+                _failureRate = Math.Abs(_failureRate);
+                if (_failureRate>35) 
+                {
+                    MessageBox.Show("Интенсивность отказов вне диаппазона допустимых значений");
+                    return false;
+                }
+            }
+            if (!double.TryParse(textBox_NumberExperiments.Text, out _numberExperiments))
+            {
+                MessageBox.Show("Некорректное значение для поля Количество испытаний");
+                return false;
+            }
+            else 
+            {
+                _numberExperiments = Math.Abs(_numberExperiments);
+            }
+
+            textBox_Time.Text = _time.ToString();
+            textBox_FailureRate.Text = _failureRate.ToString();
+            textBox_NumberExperiments.Text = _numberExperiments.ToString();
+            return true;
+        }
+
+        // Метод для вычисления факториала
+        static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+    }
+}
